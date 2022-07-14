1293c3327ec1dfb40c440042e263f0155e7a2d28
AKOG99 <108433146+AKOG99@users.noreply.github.com>
2022-07-13 21:58:46 -0500

    Se creo el archivo principal.php dentro de la carpeta modulos

---
+++ vistas/modulos/principal.php
@@ -0,0 +1,17 @@
+<div class="jumbotron">
+<h1 class="display-4">App paquetería</h1>
+  <hr class="my-4">
+  <p></p><br>
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+        <img src="vistas/img/paqueteria.jpg" class="d-block w-100" alt="..." width="200" height="500">
+        </div>
+    </div>
+    </div>
+</div>
+<br><br>
+
+
+
+
