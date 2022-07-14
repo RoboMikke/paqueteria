bc076e21ee436e9baca7b0f2d9830c024dd72c72
AKOG99 <108433146+AKOG99@users.noreply.github.com>
2022-07-13 22:03:00 -0500

    Se creo el archivo menu.php dentro de la carpeta modulos

---
+++ vistas/modulos/menu.php
@@ -0,0 +1,47 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+  
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="index.php?opcion=principal">Principal</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Altas
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="index.php?opcion=alta_camion">Camión</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=alta_camionero">Camionero</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=alta_camionero_camion">Camionero camión</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=alta_destinatario">Destinatario</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=alta_paquete">Paquete</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=alta_poblacion">Población</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=alta_provincia">Provincia</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=alta_rol">Rol</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=alta_usuario">Usuario</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Mostrar
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="index.php?opcion=mostrar_camion">Camión</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=mostrar_camionero">Camionero</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=mostrar_camionero_camion">Camionero camión</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=mostrar_destinatario">Destinatario</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=mostrar_paquete">Paquete</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=mostrar_poblacion">Población</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=mostrar_provincia">Provincia</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=mostrar_rol">Rol</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=mostrar_usuario">Usuario</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
