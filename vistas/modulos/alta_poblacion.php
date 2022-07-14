833f76f5d0ac6466e93140e63f22fc473bcd3a3a
Ortega04 <108433221+Ortega04@users.noreply.github.com>
2022-07-14 09:29:55 -0600

    Modificacion del archivo alta_poblacion.php
    Se le agrego diseño

--- vistas/modulos/alta_poblacion.php
+++ vistas/modulos/alta_poblacion.php
@@ -1,27 +1,22 @@
-<div class="jumbotron jumbotron-fluid">
+<div class="jumbotron jumbotron-fluid" style="background-color: rgb(213, 57, 0); text-align: center; color: papayawhip;">
   <div class="container">
-    <h1 class="display-4">Alta población</h1>
-    <p class="lead">Por favor llene los campos solicitados</p>
+    <h1 class="display-4"><b><i class="fa-solid fa-file-pen"></i> Alta de poblaciones</b></h1>
+    <hr>
+    <p class="lead"><b>Por favor llene los campos solicitados</p>
   </div>
 </div>
 <br><br>
 <form method="POST" enctype="multipart/form-data" autocomplete="off">
-   <!-- codigo -->
-   <div class="mb-3">
-        <label for="pk_poblacion" class="form-label">Codigo:</label>
-        <input type="text" class="form-control" id="pk_poblacion" name="pk_poblacion" required>
-    </div>
     <!-- provincia -->
    <div class="mb-3">
         <label for="poblacion" class="form-label">Poblacion:</label>
-        <input type="text" class="form-control" id="poblacion" name="poblacion" required>
+        <input type="text" class="form-control" id="poblacion" name="poblacion" maxlength="20"  pattern="[a-zA-Z ]{2,254}"  title="Escriba solo letras y un máximo de 20 caracteres" placeholder="Santa Cruz de Teneri" required>
     </div>
     <br><br>
-    <!--boton-->
+    <!--Botón-->
     <div class="d-grid gap-2 col-6 mx-auto">
-        <button class="btn btn-primary" type="submit">Guardar</button>
-    </div>
-    <br><br>
+        <button style="background-color: rgb(213, 57, 0); color: aliceblue;; font-size: 20px; font-weight: bold;" class="btn btn-warning" type="submit"><i class="fa-solid fa-floppy-disk"></i> Guardar</button>
+    </div><br><br>
 </form>
 <?php
     $registro = new Controlador(); 
