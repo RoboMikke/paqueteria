ad53efaf3298831df8e9d25b379bf46e268d5dea
Ortega04 <108433221+Ortega04@users.noreply.github.com>
2022-07-14 09:07:08 -0600

    Modificacion del archivo alta_camionero_camion.php
    Se le agrego diseño al archivo

--- vistas/modulos/alta_camionero.php
+++ vistas/modulos/alta_camionero.php
@@ -1,7 +1,8 @@
-<div class="jumbotron jumbotron-fluid">
+<div class="jumbotron jumbotron-fluid" style="background-color: rgb(213, 57, 0); text-align: center; color: papayawhip;">
   <div class="container">
-    <h1 class="display-4">Alta camionero</h1>
-    <p class="lead">Por favor llene los campos solicitados</p>
+    <h1 class="display-4"><b><i class="fa-solid fa-file-pen"></i> Alta de camioneros</b></h1>
+    <hr>
+    <p class="lead"><b>Por favor llene los campos solicitados</p>
   </div>
 </div>
 <br><br>
@@ -9,32 +10,32 @@
     <!--dni-->
     <div class="mb-3">
         <label for="dni" class="form-label">DNI:</label>
-        <input type="text" maxlength="9" class="form-control" id="dni" name="dni" required>
+        <input type="text" class="form-control" id="dni" name="dni" maxlength="9" pattern="[0-9]*[A-Z]" title="Cumpla con el formato de 8 números y 1 letra mayúscula" placeholder="32164578J" required>
     </div>
     <!--nombre-->
      <div class="mb-3">
         <label for="nombre" class="form-label">Nombre:</label>
-        <input type="text" class="form-control" id="nombre" name="nombre" required>
+        <input type="text" class="form-control" id="nombre" name="nombre" maxlength="20"  pattern="[a-zA-Z ]{2,254}" title="Escriba solo letras y un máximo de 20 caracteres" placeholder="Jose Luis" required>
     </div>
     <!-- primer apellido -->
      <div class="mb-3">
         <label for="primer_apellido" class="form-label">Primer apellido:</label>
-        <input type="text" class="form-control" id="primer_apellido" name="primer_apellido" required>
+        <input type="text" class="form-control" id="primer_apellido" name="primer_apellido" maxlength="20"  pattern="[a-zA-Z ]{2,254}"  title="Escriba solo letras y un máximo de 20 caracteres" placeholder="Carrillo" required>
     </div>
     <!-- segundo apellido -->
      <div class="mb-3">
         <label for="segundo_apellido" class="form-label">Segundo apellido:</label>
-        <input type="text" class="form-control" id="segundo_apellido" name="segundo_apellido" required>
+        <input type="text" class="form-control" id="segundo_apellido" name="segundo_apellido" maxlength="20"  pattern="[a-zA-Z ]{2,254}"  title="Escriba solo letras y un máximo de 20 caracteres" placeholder="Hinojosa" required>
     </div>
     <!-- telefono -->
     <div class="mb-3">
         <label for="telefono" class="form-label">Telefono:</label>
-        <input type="number" class="form-control" id="telefono" name="telefono" required>
+        <input type="text" class="form-control" id="telefono" name="telefono" maxlength="10" minlength="10"   min="1" pattern="^[0-9]+"  title="Escriba solo 10 números" placeholder="3231325302" required>
     </div>
     <!-- salario -->
     <div class="mb-3">
-        <label for="salario" class="form-label">Salario:</label>
-        <input type="text" class="form-control" id="salario" name="salario" required>
+        <label for="salario" class="form-label">Salario mensual:</label>
+        <input type="number" class="form-control" id="salario" name="salario" maxlength="10"  min="1" pattern="^[0-9]+" title="Escriba solo 10 números" placeholder="2500" required>
     </div>
     <!-- poblacion -->
     <div class="mb-3">
@@ -51,12 +52,22 @@
     </select>
     </div>
     <br><br>
-    <!--boton-->
+    <!--Botón-->
     <div class="d-grid gap-2 col-6 mx-auto">
-        <button class="btn btn-primary" type="submit">Guardar</button>
-    </div>
-    <br><br>
+        <button style="background-color: rgb(213, 57, 0); color: aliceblue;; font-size: 20px; font-weight: bold;" class="btn btn-warning" type="submit"><i class="fa-solid fa-floppy-disk"></i> Guardar</button>
+    </div><br><br>
 </form>
+
+<script>
+    function mayus(e) {
+
+        var tecla=e.value;
+        var tecla2=tecla.toUpperCase();
+
+        alert(tecla2);
+        }
+</script>
+
 <?php
     $registro = new Controlador(); 
     $registro -> registroCamioneroControlador();
--- vistas/modulos/alta_camionero_camion.php
+++ vistas/modulos/alta_camionero_camion.php
@@ -1,16 +1,12 @@
-<div class="jumbotron jumbotron-fluid">
+<div class="jumbotron jumbotron-fluid" style="background-color: rgb(213, 57, 0); text-align: center; color: papayawhip;">
   <div class="container">
-    <h1 class="display-4">Alta camionero camión</h1>
-    <p class="lead">Por favor llene los campos solicitados</p>
+    <h1 class="display-4"><b><i class="fa-solid fa-file-pen"></i> Alta de camionero-camión</b></h1>
+    <hr>
+    <p class="lead"><b>Por favor llene los campos solicitados</p>
   </div>
 </div>
 <br><br>
 <form method="POST" enctype="multipart/form-data" autocomplete="off">
-   <!-- codigo -->
-   <div class="mb-3">
-        <label for="pk_camionero_camion" class="form-label">Codigo:</label>
-        <input type="text" class="form-control" id="pk_camionero_camion" name="pk_camionero_camion" required>
-    </div>
     <!-- camionero -->
     <div class="mb-3">
         <label for="fk_camionero" class="form-label">Camionero:</label>
@@ -20,21 +16,21 @@
             $consulta = Controlador::consultaCamioneroControlador();
             foreach($consulta as $datos => $valores)
             {
-                echo '<option value="'.$valores["dni"].'">'.$valores["nombre"].$valores["primer_apellido"].'</option>';
+                echo '<option value="'.$valores["dni"].'">'.$valores["nombre"].' '.$valores["primer_apellido"].'</option>';
             }
         ?>
     </select>
     </div>
     <!-- camion -->
      <div class="mb-3">
-        <label for="fk_camion" class="form-label">Camion:</label>
+        <label for="fk_camion" class="form-label">Camion/Matrícula:</label>
         <select class="form-select" aria-label="Default select example" name="fk_camion" id="fk_camion" required>
         <option value="">Seleccione...</option>
         <?php
             $consulta = Controlador::consultaCamionControlador();
             foreach($consulta as $datos => $valores)
             {
-                echo '<option value="'.$valores["matricula"].'">'.$valores["matricula"].$valores["modelo"].'</option>';
+                echo '<option value="'.$valores["matricula"].'">'.$valores["matricula"].'</option>';
             }
         ?>
     </select>
@@ -45,11 +41,10 @@
         <input type="date" class="form-control" id="fecha" name="fecha" required>
     </div>
     <br><br>
-    <!--boton-->
+    <!--Botón-->
     <div class="d-grid gap-2 col-6 mx-auto">
-        <button class="btn btn-primary" type="submit">Guardar</button>
-    </div>
-    <br><br>
+        <button style="background-color: rgb(213, 57, 0); color: aliceblue;; font-size: 20px; font-weight: bold;" class="btn btn-warning" type="submit"><i class="fa-solid fa-floppy-disk"></i> Guardar</button>
+    </div><br><br>
 </form>
 <?php
     $registro = new Controlador(); 
