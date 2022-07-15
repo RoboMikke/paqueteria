e3ffdf290fc614effce631e06d9140d383fd65f7
RoboMikke <miguelangelacostapalacios91@gmail.com>
2022-07-14 18:54:24 -0600

    ActualiAzacion mostrar_camion.php

--- vistas/modulos/mostrar_camion.php
+++ vistas/modulos/mostrar_camion.php
@@ -1,17 +1,19 @@
-<div class="jumbotron jumbotron-fluid">
+<div class="jumbotron jumbotron-fluid" style="background-color: rgb(213, 57, 0); text-align: center; color: papayawhip;">
   <div class="container">
-    <h1 class="display-4">Listado de Camiones</h1>
+    <h1 class="display-4"><b><i class="fa-solid fa-clipboard-list"></i> Listado de Camiones</b></h1>
+    <hr>
+    <p class="lead"><b>Paquetería España</b></p>
   </div>
 </div>
 <br><br>
 <div>
-    <table class="table table-hover table-striped">
+    <table  id="example" style="width:100%" class="table table-hover table-striped">
         <thead>
             <tr>
-                <th>Matricula</th>
-                <th>Modelo</th>
-                <th>Tipo</th>
-                <th>Potencia</th>
+                <th>Matricula del Camión</th>
+                <th>Modelo del Camión</th>
+                <th>Tipo de Camión</th>
+                <th>Potencia HP</th>
             </tr>
         </thead>
         <tbody>
@@ -22,3 +24,4 @@
         </tbody>
     </table>
 </div>
+
