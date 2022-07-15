4977b605ee9c5ea5fd559205472ae062b824047c
RoboMikke <miguelangelacostapalacios91@gmail.com>
2022-07-14 18:47:39 -0600

    Actualizacion menu.php

--- vistas/modulos/menu.php
+++ vistas/modulos/menu.php
@@ -1,5 +1,6 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
+    <a class="navbar-brand"><i class="fa-solid fa-truck-fast"></i> Paquetería España</a>
   
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -7,38 +8,36 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link" href="index.php?opcion=principal">Principal</a>
+          <a class="nav-link" href="index.php?opcion=principal"><i class="fa-solid fa-house"></i> Principal</a>
         </li>
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Altas
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fa-solid fa-file-pen"></i>  Altas
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="index.php?opcion=alta_camion">Camión</a></li>
-            <li><a class="dropdown-item" href="index.php?opcion=alta_camionero">Camionero</a></li>
-            <li><a class="dropdown-item" href="index.php?opcion=alta_camionero_camion">Camionero camión</a></li>
-            <li><a class="dropdown-item" href="index.php?opcion=alta_destinatario">Destinatario</a></li>
-            <li><a class="dropdown-item" href="index.php?opcion=alta_paquete">Paquete</a></li>
-            <li><a class="dropdown-item" href="index.php?opcion=alta_poblacion">Población</a></li>
-            <li><a class="dropdown-item" href="index.php?opcion=alta_provincia">Provincia</a></li>
-            <li><a class="dropdown-item" href="index.php?opcion=alta_rol">Rol</a></li>
-            <li><a class="dropdown-item" href="index.php?opcion=alta_usuario">Usuario</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=alta_camion"><i class="fa-solid fa-file-pen"></i> Camión</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=alta_camionero"><i class="fa-solid fa-file-pen"></i> Camionero</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=alta_camionero_camion"><i class="fa-solid fa-file-pen"></i> Camionero camión</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=alta_destinatario"><i class="fa-solid fa-file-pen"></i> Destinatario</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=alta_paquete"><i class="fa-solid fa-file-pen"></i> Paquete</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=alta_poblacion"><i class="fa-solid fa-file-pen"></i> Población</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=alta_provincia"><i class="fa-solid fa-file-pen"></i> Provincia</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=alta_rol"><i class="fa-solid fa-file-pen"></i> Rol</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=alta_usuario"><i class="fa-solid fa-file-pen"></i> Usuario</a></li>
           </ul>
         </li>
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Mostrar
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fa-solid fa-clipboard-list"></i> Mostrar
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="index.php?opcion=mostrar_camion">Camión</a></li>
-            <li><a class="dropdown-item" href="index.php?opcion=mostrar_camionero">Camionero</a></li>
-            <li><a class="dropdown-item" href="index.php?opcion=mostrar_camionero_camion">Camionero camión</a></li>
-            <li><a class="dropdown-item" href="index.php?opcion=mostrar_destinatario">Destinatario</a></li>
-            <li><a class="dropdown-item" href="index.php?opcion=mostrar_paquete">Paquete</a></li>
-            <li><a class="dropdown-item" href="index.php?opcion=mostrar_poblacion">Población</a></li>
-            <li><a class="dropdown-item" href="index.php?opcion=mostrar_provincia">Provincia</a></li>
-            <li><a class="dropdown-item" href="index.php?opcion=mostrar_rol">Rol</a></li>
-            <li><a class="dropdown-item" href="index.php?opcion=mostrar_usuario">Usuario</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=mostrar_camion"><i class="fa-solid fa-clipboard-list"></i>  Camión</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=mostrar_camionero"><i class="fa-solid fa-clipboard-list"></i>  Camionero</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=mostrar_camionero_camion"><i class="fa-solid fa-clipboard-list"></i>  Camionero camión</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=mostrar_destinatario"><i class="fa-solid fa-clipboard-list"></i>  Destinatario</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=mostrar_paquete"><i class="fa-solid fa-clipboard-list"></i>  Paquete</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=mostrar_poblacion"><i class="fa-solid fa-clipboard-list"></i>  Población</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=mostrar_provincia"><i class="fa-solid fa-clipboard-list"></i>  Provincia</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=mostrar_rol"><i class="fa-solid fa-clipboard-list"></i>  Rol</a></li>
+            <li><a class="dropdown-item" href="index.php?opcion=mostrar_usuario"><i class="fa-solid fa-clipboard-list"></i>  Usuario</a></li>
           </ul>
         </li>
       </ul>
