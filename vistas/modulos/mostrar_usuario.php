337b037441ef2b500b7963938a69a23448b245f3
RoboMikke <miguelangelacostapalacios91@gmail.com>
2022-07-14 19:10:29 -0600

    Actualizar mostrar_usuario.php

--- vistas/modulos/mostrar_usuario.php
+++ vistas/modulos/mostrar_usuario.php
@@ -1,14 +1,17 @@
-<div class="jumbotron jumbotron-fluid">
+<div class="jumbotron jumbotron-fluid" style="background-color: rgb(213, 57, 0); text-align: center; color: papayawhip;">
   <div class="container">
-    <h1 class="display-4">Listado de Usuarios</h1>
+    <h1 class="display-4"><b><i class="fa-solid fa-clipboard-list"></i> Listado de Usuarios</b></h1>
+    <hr>
+    <p class="lead"><b>Paquetería España</b></p>
   </div>
 </div>
 <br><br>
 <div>
-    <table class="table table-hover table-striped">
+    <table id="example" style="width:100%" class="table table-hover table-striped">
         <thead>
             <tr>
                 <th>Usuario</th>
+                <th>Password</th>
                 <th>Rol</th>
             </tr>
         </thead>
