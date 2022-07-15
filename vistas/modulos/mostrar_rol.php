e599442c7fad0697a20800c910f84ec315fe765e
RoboMikke <miguelangelacostapalacios91@gmail.com>
2022-07-14 19:09:25 -0600

    Actualizacion mostrar_rol.php

--- vistas/modulos/mostrar_rol.php
+++ vistas/modulos/mostrar_rol.php
@@ -1,11 +1,13 @@
-<div class="jumbotron jumbotron-fluid">
+<div class="jumbotron jumbotron-fluid" style="background-color: rgb(213, 57, 0); text-align: center; color: papayawhip;">
   <div class="container">
-    <h1 class="display-4">Listado de Roles</h1>
+    <h1 class="display-4"><b><i class="fa-solid fa-clipboard-list"></i> Listado de Roles</b></h1>
+    <hr>
+    <p class="lead"><b>Paquetería España</b></p>
   </div>
 </div>
 <br><br>
 <div>
-    <table class="table table-hover table-striped">
+    <table id="example" style="width:100%" class="table table-hover table-striped">
         <thead>
             <tr>
                 <th>Código</th>
